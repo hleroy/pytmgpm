ed9926cae815af2b1f58c6bed44d444724743e1e
Hervé Le Roy <hleroy@hleroy.com>
2014-08-19 15:15:39 +0200

    Added some tests

--- tests.py
+++ tests.py
@@ -1,6 +1,73 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import unittest
+import datetime
 from tmgpm import Tmgpm
 
-#TODO
+
+class TestTmgpmDefaultConstructor(unittest.TestCase):
+
+    def setUp(self):
+        self.tide = Tmgpm()
+
+    def test_default_station(self):
+        # check default station name is BREST       
+        self.assertEqual(self.tide.station_name, 'BREST')
+
+    def test_default_date(self):
+        # check default date is today
+        today = datetime.date.today()
+        self.assertEqual(datetime.date(self.tide.year, self.tide.month, self.tide.day), today)
+
+
+class TestTmgpmConstructor(unittest.TestCase):
+
+    def setUp(self):
+        self.tide = Tmgpm('CONCARNEAU', 2014, 1, 1)
+
+    def test_default_station(self):
+        # check station name is properly set       
+        self.assertEqual(self.tide.station_name, 'CONCARNEAU')
+
+    def test_default_date(self):
+        # check date is properly set
+        self.assertEqual(self.tide.year, 2014)
+        self.assertEqual(self.tide.month, 1)
+        self.assertEqual(self.tide.day, 1)
+
+
+class TestTmgpmSetDate(unittest.TestCase):
+
+    def setUp(self):
+        self.tide = Tmgpm()
+
+    def test_date_before_1900(self):
+        # check that a date before 1/1/1900 before is refused
+        self.assertRaises(ValueError, self.tide.set_date, 1899, 12, 31)
+
+    def test_date_after_2100(self):
+        # check default date is today
+        self.assertRaises(ValueError, self.tide.set_date, 2100, 3, 1)
+
+class TestTmgpmGetStationList(unittest.TestCase):
+
+    def test_returns_a_list(self):
+        # check we get a list
+        station_list = Tmgpm.get_station_list()
+        self.assertTrue(type(station_list) is list)
+        
+    def test_list_is_not_empty(self):
+        # check the list is not empty
+        station_list = Tmgpm.get_station_list()
+        self.assertTrue(len(station_list) > 0)
+        
+    def test_list_contains_brest(self):
+        # check the list contains at least BREST station
+        station_list = Tmgpm.get_station_list()
+        self.assertTrue('BREST' in station_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
